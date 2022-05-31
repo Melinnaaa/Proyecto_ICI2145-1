a9ee10ce0dd1e11dccadf31d15d8b533479faeeb
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-05-31 00:22:38 -0400

    Se leen los movimientos y sea guardan en el mapa

--- src/pokemon.c
+++ src/pokemon.c
@@ -47,7 +47,48 @@ void loadPokemons()
     fclose(pokemon);
 }
 
-void loadMovements();
+void loadMovements(HashMap* movementMap)
 {
-    
+    FILE* moves = fopen ("move-data-clean.csv", "r");
+    char linea[1024];
+
+    //Se lee el archivo csv linea por linea.
+    while (fgets (linea, 1023, moves) != NULL) 
+    {
+        Movement* tmpMove = malloc(sizeof(Movement));
+        for (int i = 1 ; i < 6 ; i = i + 1) //Itera 4 veces por linea
+        {
+            char* aux = get_csv_field(linea, i); // Se guardan los datos en la variable auxiliar
+            switch (i)
+            {
+                case 1://Se guarda el nombre
+                {
+                    strcpy(tmpMove->name, aux);
+                    break;
+                }
+                case 2://Se guarda el tipo
+                {
+                    strcpy(tmpMove->type, aux);
+                    break;
+                }
+                case 3:// Se guardan los power points
+                {
+                    tmpMove->pp = atoi(aux);
+                    break;
+                }
+                case 4:// Se guarda el daño
+                {
+                    tmpMove->damage = atoi(aux);
+                    break;
+                }
+                case 5:// Se guarda el % de acierto
+                {
+                    tmpMove->accuracy = atoi(aux);
+                    tmpMove->accuracy /= 100;
+                }
+            }  
+        }
+        insertMap(movementMap, tmpMove->name, tmpMove);
+    }
+    fclose(moves);
 }
