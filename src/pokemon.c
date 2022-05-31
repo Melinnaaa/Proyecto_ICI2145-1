19b8abf36c676fea9f1571c3dd1717a89aff95ba
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-05-30 23:35:48 -0400

    Se agrega función que exporta datos de pokemons

--- src/pokemon.c
+++ src/pokemon.c
@@ -1,6 +1,44 @@
 #include "pokemon.h"
+#include  <stdio.h>
 
-void loadMovements()
+void loadPokemons()
 {
-    return;
+    FILE* pokemon = fopen ("out.csv", "r");
+    char linea[1024];
+    int largo;
+
+    //Se lee el archivo csv linea por linea.
+    while (fgets (linea, 1023, pokemon) != NULL) 
+    {
+        Pokemon* tmpPkm = malloc(sizeof(Pokemon));
+        for (int i = 0 ; i < 4 ; i = i + 1) //Itera 5 veces por linea, ya que una canción solo tiene 5 caracteristicas.
+        {
+            char* aux = get_csv_field(linea, i); // Se guardan los datos en la variable auxiliar
+
+            switch (i)
+            {
+                case 0:
+                {
+                    tmpPkm->ID = atoi(aux);
+                    break;
+                }
+                case 1:
+                {
+                    strcpy(tmpPkm->name, aux);
+                    break;
+                }
+                case 2:
+                {
+                    tmpPkm->type =
+                    break;
+                }
+                case 3:
+                {
+
+                    break;
+                }
+            }  
+        }
+        
+    }
 }
