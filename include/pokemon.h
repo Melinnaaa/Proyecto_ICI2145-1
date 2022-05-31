171cf9f303d25204b1df196858212239981ee279
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-05-30 22:49:21 -0400

    Se añaden los structs y funciones

--- include/pokemon.h
+++ include/pokemon.h
@@ -1,9 +1,21 @@
 #ifndef POKEMON_H
 #define POKEMON_H
 
-asd
-typedef struct {
+typedef struct Movement {
+    char name[30];
+    char type[30];
+    int pp;
+    int damage;
+    float accuracy;
+} Movement;
 
+typedef struct Pokemon{
+char type[30];
+char name[30];
+Movement* moves[4];
 } Pokemon;
+
+void loadMovements();
+void loadPokemons();
 
 #endif
