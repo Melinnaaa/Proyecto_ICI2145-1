833fc79e5c8cb0aeed07bfff302f5828cfe457a7
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-06-13 23:47:41 -0400

    Se agregan comentarios y se crean 2 grafos explicitos

--- main.c
+++ main.c
@@ -14,49 +14,71 @@ void showPlayers();
 int main() {
     srand(time(NULL));
     printf("Bienvenido a pokemon.exe\n");
+    
+    //Mapa de movimientos con clave a partir del tipo.
     HashMap* movementsStr = createMap(218);
+
+    //Mapa de pokemons con clave a partir de su nombre.
     HashMap* pokemonsStr = createMap(917);
+
+    //Mapa de movimientos con clave a partir de su nombre.
     HashMap* movements = createMap(218);
+
+    //Arreglo de items disponibles.
     Item* items = calloc(5, sizeof(Item*));
+
+    //Grafo de afinidades.
+    HashMap* effective = createMap(35);
+
+    //Grafo de debilidades.
+    HashMap* uneffective = createMap(35);
+
+    //Se cargan los pokemons, movimientos y items de archivos csv.
     loadPokemons(pokemonsStr);
     loadMovements(movementsStr, movements);
     loadItems(items);
+    getEffectiveNodes(effective);
+    getUneffectiveNodes(uneffective);
+
     int j;//Almacena el numero del jugador.
-    // Jugadores
+    int in = -1;//Opcion ingresada por el usuario.
+
+    // Se inicializan las variables de los jugadores.
     Player players[2];
     for (int i = 0; i < 2; i++) {
         createPlayer(&players[i]);
     }
-    int in = -1;
+
+    //Bucle principal del programa.
     while (in != 0) {
         showMenu();
         in = checkNum(0, 5);
         
         switch(in)  
         {
-            case 1: // Crear perfil
+            case 1: // Crear perfil.
             {
                 //Se recibe el numero en donde se almacenaran los datos del jugador
                 j = getPlayerPos();
                 createProfile(&players[j-1], pokemonsStr, movementsStr);
                 break;
             }
-            case 2: // Cargar perfiles
+            case 2: // Cargar perfiles.
             {
                 login(players, pokemonsStr, movements, items);
                 break;
             }
-            case 3: // Mostrar jugadores
+            case 3: // Mostrar jugadores.
             {
                 showPlayers(players);
                 break;
             }
-            case 4:
+            case 4://Guardar los perfiles.
             {
                 sign_in(players);
                 break;
             }
-            case 5:
+            case 5://Comprar items en la tienda.
             {
                 j = getPlayerPos();
                 buyItems(items, &players[j-1]);
@@ -85,11 +107,12 @@ void createPlayer(Player *p)
  
 }
 
-//menu
+//Iterar jugadores.
 void showPlayers(Player *players)
 {   
     for (int i = 0; i < 2; i++)
     {
+        //Si el jugador existe se muestran sus datos.
         if(players[i].pokemons->ptr != NULL)
             showPlayer(&players[i]);
         else
@@ -97,36 +120,33 @@ void showPlayers(Player *players)
     }
 }
 
+//Mostrar datos del jugador.
 void showPlayer(Player *p)
 {
-    printf("Jugador %s\n", p->name);
-    printf("Victorias %d\n", p->wins);
-    printf("Derrotas %d\n", p->losses);
-    printf("Ratio victorias / derrotas %f\n", (p->losses && p->wins)  ?
+    printf("Jugador: %s\n", p->name);
+    printf("Victorias: %d\n", p->wins);
+    printf("Derrotas: %d\n", p->losses);
+    printf("Ratio victorias / derrotas: %f\n", (p->losses && p->wins)  ?
             ((float)p->wins / p->losses) : 0 );
-    // Inventario
+    printf("Dinero: %d", p->money);
+    //Se muestra el inventario si tiene objetos.
     for(int i = 0; i < 5; i++) {
         if(p->inventory[i].item != NULL) {
-            printf("a\n");
             showItem(p->inventory[i].item);
         }
     }
 
+    //Se muestran los pokemons del jugador.
     for(int i = 0; i < 4; i++) 
     {
         showPlayerPokemon(&p->pokemons[i]);
     }
 }
 
-//TODO
-void loadProfiles()
-{
-    return;
-}
-
-//TODO
+//Muestra el item del jugador.
 void showItem(Item *item)
 {
+    printf("%s\n", item->name);
     return;
 }
 
