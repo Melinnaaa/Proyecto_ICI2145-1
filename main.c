64557551ba14804e62675f03f3bf2384c57347a8
Benjamín Robles <benjamin.robles.a@mail.pucv.cl>
2022-06-16 13:52:13 -0400

    eliminar definicion del main.c

--- main.c
+++ main.c
@@ -108,41 +108,7 @@ void createPlayer(Player *p)
  
 }
 
-//Iterar jugadores.
-void showPlayers(Player *players)
-{   
-    for (int i = 0; i < 2; i++)
-    {
-        //Si el jugador existe se muestran sus datos.
-        if(players[i].pokemons->ptr != NULL)
-            showPlayer(&players[i]);
-        else
-            printf("El jugador %d no se ha registrado.\n\n", i + 1);
-    }
-}
 
-//Mostrar datos del jugador.
-void showPlayer(Player *p)
-{
-    printf("Jugador: %s\n", p->name);
-    printf("Victorias: %d\n", p->wins);
-    printf("Derrotas: %d\n", p->losses);
-    printf("Ratio victorias / derrotas: %f\n", (p->losses && p->wins)  ?
-            ((float)p->wins / p->losses) : 0 );
-    printf("Dinero: %d", p->money);
-    //Se muestra el inventario si tiene objetos.
-    for(int i = 0; i < 5; i++) {
-        if(p->inventory[i].item != NULL) {
-            showItem(p->inventory[i].item);
-        }
-    }
-
-    //Se muestran los pokemons del jugador.
-    for(int i = 0; i < 4; i++) 
-    {
-        showPlayerPokemon(&p->pokemons[i]);
-    }
-}
 
 //Muestra el item del jugador.
 void showItem(Item *item)
