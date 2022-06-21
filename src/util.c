0771c63089fd1af8ab84b9d59fa817a97fedcb74
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-06-21 13:26:31 -0400

    Se comenta funcion

--- src/util.c
+++ src/util.c
@@ -119,13 +119,14 @@ int randomNumber(int lowerbound, int upperbound)
     return (rand() % (upperbound - lowerbound + 1) + lowerbound);
 }
 
-unsigned long hash(unsigned char *str)
+/*unsigned long hash(unsigned char *str)
 {
     unsigned long hash = 5381;
     int c;
 
     while (c = *str++)
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+        hash = ((hash << 5) + hash) + c; // hash * 33 + c
 
     return hash;
-}
+}*/
+
