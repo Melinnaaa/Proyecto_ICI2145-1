ab6f1ad5f336c73f44c76dc53ea6a2734dba1e29
Arturo Reyes <arturo.reyes.c01@mail.pucv.cl>
2022-06-25 00:41:42 -0400

    Se crea el combate de cpu

--- include/cpu.h
+++ include/cpu.h
@@ -1,15 +1,17 @@
 #ifndef cpu_h
 #define cpu_h
 #include "player.h"
+#include "combat.h"
+#include "hashmap.h"
 
-typedef struct
-{
-    Player player;
-    HashMap* effective;
-    HashMap* uneffective;
-}Cpu;
+struct CpuCombat {
+    struct Combat;
+    Player* player;
+};
 
 void fileToPlayer(Player* dest, char* name, HashMap* pokemon, HashMap* moves);
-Cpu createCPU (HashMap* pokemon, HashMap* moves);
+Player createCPU (HashMap* pokemon, HashMap* moves);
+void initCpuCombat (Player* players, Player* cpu, HashMap* effective, HashMap* uneffective);
+
 
 #endif
--- src/cpu.c
+++ src/cpu.c
@@ -127,10 +127,10 @@ void fileToPlayer(Player* dest, char* name, HashMap* pokemon, HashMap* moves)
 }
 
 
-Cpu createCPU (HashMap* pokemon, HashMap* moves)
+Player createCPU (HashMap* pokemon, HashMap* moves)
 {
     DIR* dirp;
-    Cpu tmp;
+    Player cpu;
     int numArchives = countArchives();
     int random = randomNumber(0, numArchives-1);
     struct dirent * entry;
@@ -151,8 +151,260 @@ Cpu createCPU (HashMap* pokemon, HashMap* moves)
     }
 
     printf("%s\n", entry->d_name);
-    fileToPlayer(&tmp.player,entry->d_name, pokemon, moves);
+    fileToPlayer(&cpu,entry->d_name, pokemon, moves);
     closedir(dirp);
+    return cpu;
+}
 
-    return tmp;
+
+void initCpuCombat (Player* players, Player* cpu, HashMap* effective, HashMap* uneffective)
+{
+    if (!(players->canPlay) || !(cpu->canPlay))
+    {
+        printf("Para iniciar un combate es necesario cargar los perfiles.\n");
+        printf("Pulsa una tecla para continuar..");
+        getchar();
+        return;
+    }
+    struct Combat combat;
+
+    // Guardar los punteros de los mapas.
+    combat.maps.effective = effective;
+    combat.maps.uneffective = uneffective;
+
+    combat.shouldClose = 0;
+
+    // Determinar el primer turno.
+    combat.turn.current.ptr = cpu;
+    combat.turn.enemy.ptr = players;
+
+    combat.turn.current.selection = cpu->pokemons;
+    combat.turn.current.selectionIndex = 0;
+
+    //Guardar los pps
+    for (int i = 0 ; i < 4 ; i++)
+    {
+        for (int k = 0 ; k < 4 ; k++)
+        {
+            combat.turn.enemy.ptr->pokemons[i].pps[k] = combat.turn.enemy.ptr->pokemons[i].movements[k]->pp;
+            combat.turn.current.ptr->pokemons[i].pps[k] = combat.turn.current.ptr->pokemons[i].movements[k]->pp;
+        }
+    }
+
+    for (int i = 0; i < 4; i++)
+        combat.turn.current.consumed[i] = 0;
+
+    // Nadie ha ganado aún.
+    combat.winner = NULL;
+
+    // mientras el combate no deba acabar.
+    while (!(combat.shouldClose))
+    {
+        int j;
+        if(combat.turn.current.ptr != cpu) 
+        {
+            showCombatMenu(&combat);
+        }
+        if(combat.turn.current.ptr == cpu)
+        {
+            j = 1;
+        }
+        else j = checkNum(1, 3);
+        int k;//Pokemon a atacar.
+        int l;//Ataque a realizar.
+#ifdef DEBUG
+        printf("DEBUG: menú combate j = %d\n", j);
+#endif
+        switch (j)
+        {
+            case 0: // Huir
+                tryToEscape(&combat);
+                break;
+            case 1: // Atacar
+            {
+                Movement *movement;
+                PlayerPokemon *pkm;
+                if (combat.turn.current.ptr != cpu)
+                {
+                    printf("%s está al frente.\n", combat.turn.current.selection->ptr->name);
+                    printf("Elige tu ataque\n");
+                    showAttacks(&combat);
+repeat:
+                    l = checkNum(1, 4);
+                    if (l == 0) continue;
+                    movement = (combat.turn.current.selection->movements)[l - 1];
+                    if (combat.turn.current.selection->pps[l-1] == 0)
+                    {
+                        printf("No le quedan pps al ataque. Selecciona otro ataque.\n");
+                        goto repeat;
+                    }
+                    printf("A quién quieres atacar?\n");
+                    showAttackable(&combat);
+reask:
+                    k = checkNum(1,4);
+                    if (k == 0) continue;
+
+                    pkm = getEnemyPokemon(&combat, k - 1);
+
+                    if ( ! (pkm->hp) )
+                    {
+                        printf("%s está muerto. Elige otro objetivo.\n", pkm->ptr->name);
+                        goto reask;
+                    }
+                }
+                else
+                {
+                    printf("Turno de %s:\n\n", combat.turn.current.ptr->name);
+                    do
+                    {
+                        l = randomNumber(0,3);
+                        movement = combat.turn.current.selection->movements[l];
+                    }while (combat.turn.current.selection->pps[l] == 0);
+
+                    do
+                    {
+                        k = randomNumber(0,3);
+                        pkm = getEnemyPokemon(&combat, k);
+                    } while (pkm->hp == 0);
+                }
+
+                printf("%s usó %s!", combat.turn.current.selection->ptr->name,
+                        movement->name);
+
+                getchar();
+                putchar('\n');
+                //Se restan los pps del ataque utilizado.
+                combat.turn.current.selection->pps[l-1] -=1;
+                int flag = 1;
+                List *types = pkm->ptr->type;
+                float damage = movement->damage*0.25;
+                for (char *type = listFirst(types);
+                        type;
+                        type = listNext(types)) {
+                    // Si es efectivo pasamos al siguiente pokémon,
+                    if (isEffective(&combat, movement->type, type) && flag)
+                    {
+                        printf("Es super efectivo!");
+                        damage *= 1.5;
+                        if (combat.turn.enemy.knocked[k-1] == 1)
+                        {
+                            combat.turn.current.selection->consumed = 1;
+                            combat.turn.current.consumed[combat.turn.current.selectionIndex] = 1;
+                        }
+                        else
+                        {
+                            flag = 0;
+                            combat.turn.enemy.knocked[k-1] = 1;
+
+                            // deshabilitamos el pokemon actual para atacar.
+                            combat.turn.current.selection->consumed = 1;
+                            combat.turn.current.consumed[combat.turn.current.selectionIndex] = 1;
+
+                            getchar();
+                            putchar('\n');
+                            printf("%s está noqueado!", pkm->ptr->name);
+                            getchar();
+                            putchar('\n');
+                        }
+                        
+                    // si no es efectivo, terminamos el turno.
+                    } else if (isUneffective(&combat, movement->type, type) && flag){
+                        printf("No hizo nada..");
+                        damage = 0;
+                        flag = 0;
+                        updateTurn(&combat);
+                        getchar();
+                        putchar('\n');
+                    }
+                }
+
+                if (damage)
+                    printf("%s le quitó %d HP a %s!", combat.turn.current.selection->ptr->name, 
+                            (int)damage,
+                            pkm->ptr->name);
+
+                getchar();
+                putchar('\n');
+
+
+                if ((pkm->hp - damage) <= 0)
+                {
+                    printf("%s murió.", pkm->ptr->name);
+                    getchar();
+                    putchar('\n');
+                    pkm->hp = 0;
+                } else {
+                    pkm->hp -= damage;
+                    printf("%s ahora tiene %d HP", pkm->ptr->name, pkm->hp);
+                    getchar();
+                    putchar('\n');
+
+                }
+
+                if (canAllOutAttack(&combat) == 1) 
+                {
+                    int allOut;
+                    if(combat.turn.current.ptr != cpu)
+                    {
+                        printf("Todos los pokemons estan noqueados, es tu oportunidad para realizar un All-out Attack!\n");
+                        getchar();
+                        printf("1.Si \t 2.No\n\n");
+                        allOut = checkNum(1, 2);
+                        if (allOut == 1)
+                        {     
+                            printf("Beat 'em up!\n");
+                            getchar();
+                            doAllOutAttack(&combat);
+                            if (checkEnemy(&combat) == 1);
+                            else flag = 1;
+                        }
+                        else 
+                        {
+                            if (combat.turn.current.selectionIndex == 3) flag = 1;
+                            for (int i = 0 ; i < 4 ; i++)
+                                combat.turn.enemy.knocked[i] = 0;
+                        }
+                    }
+                    //La cpu siempre realizará all-out attack
+                    else
+                    {
+                        doAllOutAttack(&combat);
+                        if (checkEnemy(&combat) == 1);
+                        else flag = 1;
+                        for (int i = 0 ; i < 4 ; i++)
+                                combat.turn.enemy.knocked[i] = 0;
+                    }
+                }
+                // ataque normal. Terminamos el turno
+                if (flag) {
+                    updateTurn(&combat);
+                    
+#ifdef DEBUG
+                    printf("DEBUG: Ataque normal\n");
+#endif
+                } else
+                    nextSelection(&combat);
+                break;
+            }
+            case 2:
+            {
+                checkBag(&combat);
+            }
+        }
+    }
+
+    if (!combat.winner)
+    {
+        printf("Algo salió mal. (El combate terminó y no hay ganador)\n");
+    } else {
+        printf("El ganador es %s!\n", combat.winner->name);
+    }
+    for (int j = 0; j < 2; j++)
+        for (int i = 0; i < 4; i++)
+        {
+            players->pokemons[i].hp = players->pokemons[i].ptr->HP;
+            players->pokemons[i].consumed = 0;
+            for (int k = 0; k < 4; k++)
+                players->pokemons[i].pps[k] = players->pokemons[i].movements[k]->pp;
+        }
 }
